e836fa21eb30f5f060a0e923bf1e368dde23c848
rbo13 <chardyy.orofeo@gmail.com>
2022-05-30 20:42:28 +0800

    added implementation to delete message inbox using message id
    Signed-off-by: rbo13 <chardyy.orofeo@gmail.com>

--- src/blackbox.test.ts
+++ src/blackbox.test.ts
@@ -1,5 +1,5 @@
 import { Authenticate } from "./blaggo";
-import { GetInboxMessages } from "./blackbox";
+import { DeleteInboxMessages, GetInboxMessages } from "./blackbox";
 import 'dotenv/config';
 
 import * as chai from 'chai';
@@ -29,4 +29,29 @@ describe('Blackbox Test', () => {
     expect(messagesResponse.messages).to.not.be.empty;
     expect(messagesResponse.count).to.be.equal(0);
   });
+
+  it('should delete inbox message given by a message id', async() => {
+    const username = process.env["AUTH_USERNAME"] || "";
+  	const password = process.env["AUTH_PASSWORD"] || "";
+  	const auth_url = process.env["AUTH_URL"] || "";
+	  const blackbox_base_url = process.env["BLACKBOX_BASE_URL"] || "";
+
+		expect(username).to.not.be.empty;
+		expect(password).to.not.be.empty;
+		expect(auth_url).to.not.be.empty;
+
+    // authenticate
+    const response = await Authenticate(auth_url, username, password);
+    expect(response).to.not.be.null;
+    expect(response.data.tokens.access_token);
+
+    const access_token = response.data.tokens.access_token;
+    // get inbox message
+    const inboxResponse = await GetInboxMessages(`${blackbox_base_url}/inbox`, access_token);
+    const inboxId = inboxResponse.messages[0]?.id;
+    expect(inboxId).to.not.be.empty;
+
+    // delete the inbox message
+    await DeleteInboxMessages(`${blackbox_base_url}/inbox?id=${inboxId}`, access_token);
+  })
 })
--- src/blackbox/index.ts
+++ src/blackbox/index.ts
@@ -18,3 +18,36 @@ export async function GetInboxMessages(url: string, blaggoToken: string): Promis
     }
   })
 }
+
+// DeleteInboxMessages will delete a message inbox from a given:
+// id: string
+// sender_id: string
+// receiver_id: string
+// status: string {1-Sent, 2-Received, 3-Read, 4-Replied}
+// type: string {
+//   1-SubscriptionType,
+//   2-TransactionType,
+//   3-CreditType,
+//   4-BillingType,
+//   5-QRType,
+//   6-ReferralType,
+//   7-BroadcastType
+// }.
+// url = ${HOST}/inbox?id=${id}
+export async function DeleteInboxMessages(url: string, blaggoToken: string): Promise<InboxResponse> {
+  const response = await got.delete(url, {
+    headers: {
+      Authorization: `Bearer ${blaggoToken}`
+    }
+  }).json();
+
+  return new Promise((resolve, reject) => {
+    try {
+      const responseString = JSON.stringify(response);
+      let authResponse: InboxResponse = JSON.parse(responseString);
+      return resolve(authResponse);
+    } catch (error) {
+      return reject(error);
+    }
+  })
+}
